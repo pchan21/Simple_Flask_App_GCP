eedef545a040fc7816c2ae575c4b90806c745805
pchan21 <93653310+pchan21@users.noreply.github.com>
2022-12-24 22:47:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,10 +13,11 @@ dict_units_input = {'Émissions de CO2': 'g/km', 'Consommation mixte': 'l/100km'
 
 @app.route("/")
 def hello_world():
+    today = '2022-12-24'
     data_values_encoding = pickle.load(open(f'Values_Input/dict_specific_values.pickle','rb'))
     data_name_input = pickle.load(open(f'Entrees/list_input.pickle','rb'))
     data_name_encoding = pickle.load(open(f'Entrees/list_input_encoding.pickle','rb'))
-    Dataframe = pd.read_csv('Data/X_train.csv', index_col=False)
+    Dataframe = pd.read_csv(f'Data/X_train_{today}.csv', index_col=False)
     Dataframe = Dataframe.drop(['Unnamed: 0.1', 'Unnamed: 0'], axis=1)
     score = round(float(pickle.load(open('Modele/best_model_score.txt', 'rb'))),2)
     return render_template('home.html', score=score, data_name_input=data_name_input,data_values_encoding=data_values_encoding,data_name_encoding=data_name_encoding, dict_units_input=dict_units_input)
