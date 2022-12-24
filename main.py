cc492ba94fb98975051281b69da2d4261beb067c
pchan21 <93653310+pchan21@users.noreply.github.com>
2022-12-24 22:31:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -24,9 +24,9 @@ def hello_world():
 	
 @app.route('/predict',methods = ['POST'])
 def predict():
-    with open('Entrees/last_modele_date.txt') as f:
-	lines = f.readlines()
-    today = lines[0]	
+    #with open('Entrees/last_modele_date.txt') as f:
+    #	lines = f.readlines()
+    today = '2022-12-24' #lines[0]	
     data_values_encoding = pickle.load(open(f'Values_Input/dict_specific_values.pickle','rb'))
     data_name_input = pickle.load(open(f'Entrees/list_input.pickle','rb'))
     data_name_encoding = pickle.load(open(f'Entrees/list_input_encoding.pickle','rb'))
